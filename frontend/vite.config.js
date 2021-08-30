957e0b36d8be7cb06935ca727b1139c99d7e04d8
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2021-08-30 12:52:02 +0200

    small fixes with config v3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig((mode) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
+  const host = process.env.VITE_FRONTEND_HOST || "0.0.0.0";
+  const port = process.env.VITE_FRONTEND_PORT || 8080;
+
   return {
     plugins: [vue()],
     server: {
-      host: process.env.VITE_FRONTEND_HOST,
-      port: process.env.VITE_FRONTEND_PORT,
+      host: host,
+      port: port,
     },
     build: {
       rollupOptions: {
