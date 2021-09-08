238198e4e9cfdb5c0ce96d7add83af9cd1dfff90
j000 <jarymut+github@gmail.com>
2021-09-08 15:22:35 +0200

    SQL Schema: user_id must reference user or be null

--- backend/database.sql
+++ backend/database.sql
@@ -1,53 +1,53 @@
 CREATE TABLE users
 (
-    id            integer PRIMARY KEY AUTOINCREMENT,
-    name          string,
-    password_hash string
+  id            integer PRIMARY KEY AUTOINCREMENT,
+  name          string,
+  password_hash string
 );
 
 CREATE TABLE tokens
 (
-    id      integer PRIMARY KEY AUTOINCREMENT,
-    user_id integer,
-    token   string UNIQUE
+  id      integer PRIMARY KEY AUTOINCREMENT,
+  user_id integer NOT NULL REFERENCES users,
+  token   string UNIQUE
 );
 
 CREATE TABLE kik
 (
-    id       integer PRIMARY KEY AUTOINCREMENT,
-    state    string,
-    user1_id integer,
-    user2_id integer,
-    joinable boolean
+  id       integer PRIMARY KEY AUTOINCREMENT,
+  state    string,
+  user1_id integer REFERENCES users,
+  user2_id integer REFERENCES users,
+  joinable boolean
 );
 
 CREATE TABLE solitaire
 (
-    id       integer PRIMARY KEY AUTOINCREMENT,
-    user_id  integer,
-    state    string,
-    joinable boolean
+  id       integer PRIMARY KEY AUTOINCREMENT,
+  user_id  integer REFERENCES users,
+  state    string,
+  joinable boolean
 );
 
 CREATE TABLE crazy_eight
 (
-    id       integer PRIMARY KEY AUTOINCREMENT,
-    state    string,
-    user1_id integer,
-    user2_id integer,
-    user3_id integer,
-    user4_id integer,
-    joinable boolean
+  id       integer PRIMARY KEY AUTOINCREMENT,
+  state    string,
+  user1_id integer REFERENCES users,
+  user2_id integer REFERENCES users,
+  user3_id integer REFERENCES users,
+  user4_id integer REFERENCES users,
+  joinable boolean
 );
 
 CREATE TABLE oczko
 (
-    id       integer PRIMARY KEY AUTOINCREMENT,
-    state    string,
-    user1_id integer,
-    user2_id integer,
-    user3_id integer,
-    user4_id integer,
-    joinable boolean
+  id       integer PRIMARY KEY AUTOINCREMENT,
+  state    string,
+  user1_id integer REFERENCES users,
+  user2_id integer REFERENCES users,
+  user3_id integer REFERENCES users,
+  user4_id integer REFERENCES users,
+  joinable boolean
 );
 
